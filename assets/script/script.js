836e928389a7f76e2e7ab97b66b7eab818521f02
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-07-13 20:55:04 -0400

    added var to JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,8 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+  var upperLower = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbersLowerUpper = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"];
+  var numberLower = "abcdefghijklmnopqrstuvwxyz1234567890"
+  var numberUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+  var specialLowerNum = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+  var specialUpperNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+  var specialLower = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+  var specialUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+  var specialLowerUpperNum = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"];
+  var password = generatePassword(event) {
+    
+  };
+  
 
   passwordText.value = password;
 
