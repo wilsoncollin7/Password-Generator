777d0afe0f1a9eb46f0ef23b69d87034c0fa2f5e
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-07-13 21:06:11 -0400

    added confirm popups when you hit the button

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+alert("synced")
+
 // Write password to the #password input
 function writePassword() {
+  
   var passwordText = document.querySelector("#password");
-  var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
-  var upperLower = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var numbersLowerUpper = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"];
-  var numberLower = "abcdefghijklmnopqrstuvwxyz1234567890"
-  var numberUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-  var specialLowerNum = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"
-  var specialUpperNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"
-  var specialLower = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@][^_`{|}~"
-  var specialUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@][^_`{|}~"
-  var specialLowerUpperNum = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~"];
-  var password = generatePassword(event) {
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upperLower = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersLowerUpper = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  var numberLower = "abcdefghijklmnopqrstuvwxyz1234567890";
+  var numberUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  var specialLowerNum = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~";
+  var specialUpperNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~";
+  var specialLower = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@][^_`{|}~";
+  var specialUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@][^_`{|}~";
+  var specialLowerUpperNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@][^_`{|}~";
+  
+  var wantLower = confirm("Do you want lower case?");
+  var wantUpper = confirm("Do you want upper case?");
+  var wantNum = confirm("Do you want numbers?");
+  var wantSpecial = confirm("Do you want special characters?");
+
+  // if (lower = false $$ )
+  
+  // var password = generatePassword(event) {
     
-  };
+  // };
   
 
   passwordText.value = password;
