24fbce30a49e393668468967ee4d509ddba97ecd
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-07-13 22:16:03 -0400

    added function and put function into if else section/cleaned up code

--- assets/script/script.js
+++ assets/script/script.js
@@ -22,42 +22,44 @@ function writePassword() {
   var wantUpper = confirm("Do you want upper case?");
   var wantNum = confirm("Do you want numbers?");
   var wantSpecial = confirm("Do you want special characters?");
+  var pass = "";
 
-  var password = function generatePass () {
-
-  }
+  function generatePass(x) {
+    var length = charNumber;
+    var charSet = x;
+    for (var i = 0, n = charSet.length; i < length; ++i) {
+        pass += charSet.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(pass)
+  };
 
   if ((wantLower === true) && (wantUpper = true) && (wantNum === true) && (wantSpecial === true)) {
-    alert("all true")
-
+    generatePass(specialLowerUpperNum);
   } else if ((wantLower === true) && (wantUpper === true) && (wantNum === false) && (wantSpecial === false)) {
-    alert("ttff")
+    generatePass(upperLower);
   }else if ((wantLower === true) && (wantUpper === false) && (wantNum === false) && (wantSpecial === false)) {
-    alert("tfff")
-  }else if ((wantLower === false) && (wantUpper === true) && (wantNum === false) && (wantSpecial === false)) {
-    alert("ftff")
+    generatePass(lower);
   } else if ((wantLower === true) && (wantUpper === false) && (wantNum === true) && (wantSpecial === false)) {
-    alert("tftf")
+    generatePass(numbersLowerUpper);
+  } else if ((wantLower === false) && (wantUpper === true) && (wantNum === false) && (wantSpecial === false)) {
+    generatePass(upper);
+  } else if ((wantLower === true) && (wantUpper === false) && (wantNum === true) && (wantSpecial === false)) {
+    generatePass(numberLower);
   } else if ((wantLower === false) && (wantUpper === true) && (wantNum === true) && (wantSpecial === false)) {
-    alert("fttf")
+    generatePass(numberUpper);
   } else if ((wantLower === true) && (wantUpper === false) && (wantNum === true) && (wantSpecial === true)) {
-    alert("tftt")
+    generatePass(specialLowerNum);
   } else if ((wantLower === false) && (wantUpper === true) && (wantNum === true) && (wantSpecial === true)) {
-    alert("fttt")
+    generatePass(specialUpperNum);
   } else if ((wantLower === true) && (wantUpper === false) && (wantNum === false) && (wantSpecial === true)) {
-    alert("tfft")
+    generatePass(specialLower);
   } else if ((wantLower === false) && (wantUpper === true) && (wantNum === false) && (wantSpecial === true)) {
-    alert("ftft")
+    generatePass(specialUpper);
   } else if ((wantLower === false) && (wantUpper === false) && (wantNum === false) && (wantSpecial === false)) {
-    alert("ffff")
+    alert("Please select an option.");
   };
   
-   
-  
-    
-  
-  
-
+  var password = pass
   passwordText.value = password;
 
 };
